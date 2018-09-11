74a13e0423cca085207617578c3ded15fc830666
Shreyas <Shreyas@192.168.1.18>
2018-09-11 23:20:32 +1000

    first Commit-shreyas

---
+++ src/boundary/Menu.java
@@ -0,0 +1,47 @@
+package boundary;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Menu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello! Welcome to Monash Fruit and Veggie store");
+		String token1 = "";
+	    try {
+			Scanner inFile1 = new Scanner(new File("user.txt"));
+			//Scanner inFile12 = new Scanner(new File("user1.txt")).useDelimiter(",\\s*");
+			List<String> temps = new ArrayList<String>();
+			
+			
+			
+			while (inFile1.hasNext()) {
+			      // find next line
+			      token1 = inFile1.next();
+			      temps.add(token1);
+			    }
+			    inFile1.close();
+			    //inFile12.close();
+			    String[] tempsArray = temps.toArray(new String[0]);
+			    
+			    for (String s : tempsArray) {
+			        System.out.println(s);
+			      }
+			    
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	    
+		
+		
+	}
+
+}
