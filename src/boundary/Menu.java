d22f67f24d13c61daac199b25fd7e69ac5e26970
Shreyas Joshi <sjos0011@student.monash.edu>
2018-10-09 13:13:03 +1100

    Menu FILE

--- src/boundary/Menu.java
+++ src/boundary/Menu.java
@@ -3,8 +3,15 @@ package boundary;
 import java.io.*;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Scanner;
+import java.util.UUID;
+
+import controller.ProductList;
+import controller.ShoppingController;
+import javafx.util.Pair;
 
 public class Menu {
 	
@@ -14,6 +21,67 @@ public class Menu {
         String filename = "members.txt";
         try
         {
+        	//Dummy Inputs
+        	
+        	
+        	String customers="shreyas";
+        	List<Pair<UUID, Double>> items = new ArrayList<Pair<UUID, Double>>();
+        	List<Pair<UUID, String>> allProducts = new ArrayList<Pair<UUID, String>>();
+			  String deliveryMethod="card";
+			  String destAddress ="monash";
+			  String paymentMethod= "card";
+			  String paymentDetails="8520852085208520-607";
+			  Boolean paymentConfirmed=false;
+			  /*items.add(new Pair<UUID, Double>(UUID.randomUUID(), 1.5));
+			  items.add(new Pair<UUID, Double>(UUID.randomUUID(), 1.9));*/
+			  
+			  allProducts.add(new Pair<UUID, String>(UUID.randomUUID(), "MANGO"));
+			  allProducts.add(new Pair<UUID, String>(UUID.randomUUID(), "APPLE"));
+			  
+			  //-------------------------
+        	Scanner sc = new Scanner(System.in);
+        	ShoppingController s= new ShoppingController();
+        	//Browsing Products
+        	
+        	ProductList prodList = new ProductList();
+        	//List<Pair<UUID, String>> allProducts = prodC.getAllProducts(prodList);  UNCOMMENT THIS!!!
+        	
+        	ListIterator<Pair<UUID,String>> itrAllProds = allProducts.listIterator();
+  	      
+  	      while(itrAllProds.hasNext()) {
+  	    	Pair<UUID,String> element = itrAllProds.next();
+  	         System.out.println(itrAllProds.previousIndex() + " " + element.getValue());
+  	      }
+  	      
+  	      System.out.println("Enter product number to add to cart");
+      	int prodIndex = sc.nextInt();  	
+        	
+        	
+        	System.out.println("Enter quantity to add product\n");
+    		double qty = sc.nextDouble();
+    	
+    		
+    		s.addProduct(allProducts.get(prodIndex).getKey(), qty);
+    		System.out.println("Product added\n");
+    		
+    		System.out.println("Here are your cart products\n");
+    		List<Pair<UUID,Double>> cartItems = s.getCartProducts();
+    		Iterator<Pair<UUID,Double>> itr = cartItems.iterator();
+    	      
+    	      while(itr.hasNext()) {
+    	         Object element = itr.next();
+    	         System.out.print(element + " ");
+    	      }
+        	
+        	
+        	
+        	System.out.println("press one to chekout");
+        	int n = sc.nextInt();
+        	if(n==1) {
+        		
+        		s.checkout(customers,cartItems,deliveryMethod,destAddress,paymentMethod,paymentDetails,paymentConfirmed);
+        	}
+        	
             FileReader inputFile = new FileReader(filename);
             try 
             {
