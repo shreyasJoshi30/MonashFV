7dfb2e7da9afc4acbc07d6037df66d27f4bae9c6
harry <harry@DESKTOP-04RM9S8>
2018-10-10 23:42:50 +1100

    update boundary (manage inventory / manage profile has not finished.)

--- src/boundary/Menu.java
+++ src/boundary/Menu.java
@@ -11,7 +11,7 @@ import java.util.*;
 public class Menu
 {
     private String loginUsername;
-    private UserList loginUser;
+    private UserList userList;
     private ReporterController reporterController;
     private Inventory inventory;
     private ShoppingController shoppingController; 
@@ -20,7 +20,7 @@ public class Menu
     public Menu()
     {
         loginUsername = "";
-        loginUser = new UserList();
+        userList = new UserList();
         reporterController = new ReporterController();
         inventory = new Inventory();
         shoppingController = new ShoppingController();
@@ -47,11 +47,39 @@ public class Menu
                 case "A8": homeUserOwnerSignup(); break;
                 case "A9": homeUserViewUserlist(); break;
                 case "A10": homeUserUnregisterUser(); break;
-                case "X": System.out.println("See you"); break;
+                case "B": homeViewInventory(); break;
+                case "B1": homeViewInventoryAddProduct(); break;
+                case "B2": homeViewInventoryCheckout(); break;                
+                case "C": homeShoppingCart(); break;
+                case "C1": homeShoppingCartChangeCart(); break;
+                case "C2": homeShoppingCartCheckout(); break;
+                case "C3": homeShoppingCartViewInventory(); break;
+                case "D": homeManageProfile(); break;
+                case "D1": homeManageProfileAddProfile(); break;
+                case "D2": homeManageProfileViewProfile(); break;
+                case "D3": homeManageProfileEditProfile(); break;
+                case "D4": homeManageProfileDeleteProfile(); break;
+                case "E": homeManageInventory(); break;
+                case "E1": homeManageInventoryAddProfile(); break;
+                case "E2": homeManageInventoryViewProfile(); break;
+                case "E3": homeManageInventoryEditProfile(); break;
+                case "E4": homeManageInventoryDeleteProfile(); break;
+                case "X": break;
             }
             menuIndex = system.nextLine().toUpperCase().trim();
         }while (!menuIndex.equals("X"));
         cloesProgram();
+    }
+    
+    public void cloesProgram()
+    {
+        loginUsername = "";
+        userList = new UserList();
+        reporterController = new ReporterController();
+        inventory = new Inventory();
+        shoppingController = new ShoppingController();
+        productlist = new ProductList();
+        System.out.println("See you next time !");
     }
     
     public void home()
@@ -61,7 +89,7 @@ public class Menu
         System.out.println("Press A. Account");
         System.out.println("Press B. Browse our products");
         System.out.println("Press C. View your shopping");      
-        if (loginUser.isOwnerLogin(loginUsername))
+        if (userList.isOwnerLogin(loginUsername))
         {
             System.out.println("Press D. Manage product profiles");
             System.out.println("Press E. Manage inventories");
@@ -75,7 +103,7 @@ public class Menu
     public void homeUser()
     {
         System.out.println("");
-        if (!loginUser.isMemberLogin(loginUsername) && !loginUser.isOwnerLogin(loginUsername))
+        if (!userList.isMemberLogin(loginUsername) && !userList.isOwnerLogin(loginUsername))
         {
             System.out.println("Press A1. Log in");
             System.out.println("Press A2. Create your account");
@@ -89,7 +117,7 @@ public class Menu
             System.out.println("Press A7. Logout");   
             System.out.println("");
         }
-        if (loginUser.isOwnerLogin(loginUsername))
+        if (userList.isOwnerLogin(loginUsername))
         {
             System.out.println("Owner's authority");
             System.out.println("Press A8 Register owner account");
@@ -108,7 +136,7 @@ public class Menu
         String enterUsername = system.nextLine().trim();
         System.out.print("Please enter your password: ");
         String enterPassword = system.nextLine().trim();
-        if (loginUser.userLogin(enterUsername, enterPassword))
+        if (userList.userLogin(enterUsername, enterPassword))
         {
            loginUsername = enterUsername;
            System.out.println("");
@@ -141,7 +169,7 @@ public class Menu
         String phoneNumber = system.nextLine().trim();
         System.out.print("Please enter your email: ");
         String email = system.nextLine().trim();
-        if (loginUser.registerMember(username, password, firstName, lastName, dob, 
+        if (userList.registerMember(username, password, firstName, lastName, dob, 
                             phoneNumber, email))
         {
             System.out.println("");
@@ -162,7 +190,7 @@ public class Menu
         Scanner system = new Scanner(System.in); 
         System.out.print("Please enter your new password: "); 
         String newPassword = system.nextLine().trim();
-        if (loginUser.changePassword(loginUsername, newPassword))
+        if (userList.changePassword(loginUsername, newPassword))
         {
             System.out.println("");
             System.out.println("Your password has been changed.");
@@ -182,7 +210,7 @@ public class Menu
         Scanner system = new Scanner(System.in); 
         System.out.print("Please enter your new email: "); 
         String newEmail = system.nextLine().trim();
-        if (loginUser.changeEmail(loginUsername, newEmail))
+        if (userList.changeEmail(loginUsername, newEmail))
         {
             System.out.println("");
             System.out.println("Your email has been changed.");
@@ -202,7 +230,7 @@ public class Menu
         Scanner system = new Scanner(System.in); 
         System.out.print("Please enter your new phone number: "); 
         String newPhoneNumber = system.nextLine().trim();
-        if (loginUser.changePhoneNumber(loginUsername, newPhoneNumber))
+        if (userList.changePhoneNumber(loginUsername, newPhoneNumber))
         {
             System.out.println("");
             System.out.println("Your phone number has been changed.");
@@ -219,7 +247,7 @@ public class Menu
     //option A6
     public void homeUserCurrentUnregister()
     {
-        if (loginUser.unregisterUser(loginUsername))
+        if (userList.unregisterUser(loginUsername))
         {
             System.out.println("");
             System.out.println("Your account has been deleted.");
@@ -236,7 +264,7 @@ public class Menu
     //option A7
     public void homeUserLogout()
     {
-        if (loginUser.userLogout(loginUsername))
+        if (userList.userLogout(loginUsername))
         {
             loginUsername = "";
             System.out.println("");
@@ -269,7 +297,7 @@ public class Menu
         String phoneNumber = system.nextLine().trim();
         System.out.print("Please enter your email: ");
         String email = system.nextLine().trim();
-        if (loginUser.registerMember(username, password, firstName, lastName, dob, 
+        if (userList.registerMember(username, password, firstName, lastName, dob, 
                             phoneNumber, email))
         {
             System.out.println("");
@@ -287,7 +315,7 @@ public class Menu
     //option A9
     public void homeUserViewUserlist()
     {
-        loginUser.viewUserlist();
+        userList.viewUserlist();
         homeUser();
     }
     
@@ -296,12 +324,12 @@ public class Menu
     {
         System.out.println("User List:");
         System.out.println("");
-        loginUser.viewUserlist();
+        userList.viewUserlist();
         System.out.println("");
         Scanner system = new Scanner(System.in); 
         System.out.print("Please enter the username of the account to be deleted: ");        
         String username = system.nextLine().trim();
-        if (loginUser.unregisterUser(username))
+        if (userList.unregisterUser(username))
         {
             System.out.println("");
             System.out.println(username + " account has been deleted.");
@@ -315,14 +343,187 @@ public class Menu
         }        
     }
     
-    public void cloesProgram()
+    //option B
+    public void homeViewInventory()
     {
-        loginUsername = "";
-        loginUser = new UserList();
-        reporterController = new ReporterController();
-        inventory = new Inventory();
-        shoppingController = new ShoppingController();
-        productlist = new ProductList();
-        System.out.println("See you next time !");
+        //Present Inventory List
+        System.out.println("");
+        Scanner system = new Scanner(System.in); 
+        System.out.println("Press B1. Add product to your shopping cart.");
+        System.out.println("Press B2. View shopping cart / Checkout");
+        System.out.println("");
+    }
+    
+    //option B1
+    public void homeViewInventoryAddProduct()
+    {
+        System.out.println("");
+        Scanner system = new Scanner(System.in); 
+        System.out.print("Enter the product ID: ");
+        String inventoryID =  system.nextLine().trim();
+        System.out.print("Enter amounts: ");
+        String amount = system.nextLine().trim();
+        //addProduct
+        System.out.println("");
+        homeViewInventory();
+    }
+    
+    //option B2
+    public void homeViewInventoryCheckout()
+    {
+        homeShoppingCart();
+    }
+    
+    //option C
+    public void homeShoppingCart()
+    {
+        //present shoppingcart
+        System.out.println("");
+        Scanner system = new Scanner(System.in); 
+        System.out.println("Press C1. Change Shopping Cart product quantity");
+        System.out.println("Press C2. Checkout");
+        System.out.println("Press C3. back to Browse products");
+        System.out.println("");
+    }
+    
+    //option C1
+    public void homeShoppingCartChangeCart()
+    {
+        System.out.println("");
+        Scanner system = new Scanner(System.in); 
+        System.out.print("Enter the product ID: ");
+        String inventoryID =  system.nextLine().trim();
+        System.out.print("Enter amounts: ");
+        String amount = system.nextLine().trim();
+        //Alter shoppingcart items
+        System.out.println("Your shopping cart has been updated.");
+        homeShoppingCart();
+    }
+    
+    //option C2
+    public void homeShoppingCartCheckout()
+    {
+        if (userList.isMemberLogin(loginUsername))
+        {
+            //Validating order
+            String customers = loginUsername;
+            System.out.println("");
+            System.out.println("Please enter your payment information...");
+            Scanner system = new Scanner(System.in); 
+            System.out.print("Enter name on card: ");
+            String name = system.nextLine().trim();
+            System.out.print("Enter card number: ");
+            String card = system.nextLine().trim();
+            System.out.print("Enter CVV: ");
+            String CVV = system.nextLine().trim();
+            System.out.print("Enter card expiry: ");
+            String expiry = system.nextLine().trim();
+            //Validate input details
+            System.out.print("Enter Shipping Address: ");
+            String destAddress = system.nextLine().trim();
+            System.out.print("Enter delivery method: ");
+            String deliveryMethod = system.nextLine().trim();
+            System.out.println("");
+            System.out.println("Press enter to CONFIRM PAYMENT");
+            System.out.print("*************ENTER*************");
+            system.nextLine().trim();
+            //payment method
+            System.out.println("");
+            System.out.println("Thank you for shopping with us. Your order has been confirmed.");
+            home();
+        }
+        else
+        {
+            System.out.print("Please log in before you checkout.");
+            homeUser();
+        }
+    }
+    
+    //option C3
+    public void homeShoppingCartViewInventory()
+    {
+        homeViewInventory();
+    }
+    
+    //option D
+    public void homeManageProfile()
+    {
+        System.out.println("");
+        Scanner system = new Scanner(System.in); 
+        System.out.println("Press D1. Add profile");   
+        System.out.println("Press D2. View profile");   
+        System.out.println("Press D3. Edit profile");   
+        System.out.println("Press D4. Delete profile");   
+        System.out.println("");
+    }
+    
+    //option D1
+    public void homeManageProfileAddProfile()
+    {
+        
+    }
+    
+    //option D2
+    public void homeManageProfileViewProfile()
+    {
+        //present all profile
+        homeManageProfile();
+    }
+    
+    //option D3
+    public void homeManageProfileEditProfile()
+    {
+        //present all profile
+    }
+    
+    //option D4
+    public void homeManageProfileDeleteProfile()
+    {
+        //present all profile
+    }
+    
+    //option E
+    public void homeManageInventory()
+    {
+        System.out.println("");
+        Scanner system = new Scanner(System.in); 
+        System.out.println("Press E1. Add inventory");   
+        System.out.println("Press E2. View inventory");   
+        System.out.println("Press E3. Edit inventory");   
+        System.out.println("Press E4. Delete inventory");   
+        System.out.println("");
+    }
+    
+    //option E1
+    public void homeManageInventoryAddProfile()
+    {
+        
+    }
+    
+    //option E2
+    public void homeManageInventoryViewProfile()
+    {
+        //present all inventory
+        homeManageInventory();
+    }
+    
+    //option E3
+    public void homeManageInventoryEditProfile()
+    {
+        //present all inventory
+    }
+    
+    //option E4
+    public void homeManageInventoryDeleteProfile()
+    {
+        //present all inventory
+    }
+    
+    //option F
+    public void homeViewSalesReport()
+    {
+        //Present Sales Report
+        System.out.println("");
+        home();
     }
 }
