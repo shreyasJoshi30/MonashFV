3fba81b4aec0c64116e76f4e797bd0fada02f7d9
harry <harry@DESKTOP-04RM9S8>
2018-10-10 17:13:41 +1100

    boundary update - user functions complete.

--- src/boundary/Menu.java
+++ src/boundary/Menu.java
@@ -1,116 +1,299 @@
 package boundary;
 
-import java.io.*;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.Scanner;
-import java.util.UUID;
+import java.util.*;
+import controller.UserList;
 
-import controller.ProductList;
-import controller.ShoppingController;
-import javafx.util.Pair;
 
-public class Menu {
-	
-	
-	public static void main(String[] args)
+public class Menu
+{
+    private String loginUsername;
+    private UserList loginUser;
+    
+    public Menu()
     {
-        String filename = "members.txt";
-        try
-        {
-        	//Dummy Inputs
-        	
-        	
-        	String customers="shreyas";
-        	List<Pair<UUID, Double>> items = new ArrayList<Pair<UUID, Double>>();
-        	List<Pair<UUID, String>> allProducts = new ArrayList<Pair<UUID, String>>();
-			  String deliveryMethod="card";
-			  String destAddress ="monash";
-			  String paymentMethod= "card";
-			  String paymentDetails="8520852085208520-607";
-			  Boolean paymentConfirmed=false;
-			  /*items.add(new Pair<UUID, Double>(UUID.randomUUID(), 1.5));
-			  items.add(new Pair<UUID, Double>(UUID.randomUUID(), 1.9));*/
-			  
-			  allProducts.add(new Pair<UUID, String>(UUID.randomUUID(), "MANGO"));
-			  allProducts.add(new Pair<UUID, String>(UUID.randomUUID(), "APPLE"));
-			  
-			  //-------------------------
-        	Scanner sc = new Scanner(System.in);
-        	ShoppingController s= new ShoppingController();
-        	//Browsing Products
-        	
-        	ProductList prodList = new ProductList();
-        	//List<Pair<UUID, String>> allProducts = prodC.getAllProducts(prodList);  UNCOMMENT THIS!!!
-        	
-        	ListIterator<Pair<UUID,String>> itrAllProds = allProducts.listIterator();
-  	      
-  	      while(itrAllProds.hasNext()) {
-  	    	Pair<UUID,String> element = itrAllProds.next();
-  	         System.out.println(itrAllProds.previousIndex() + " " + element.getValue());
-  	      }
-  	      
-  	      System.out.println("Enter product number to add to cart");
-      	int prodIndex = sc.nextInt();  	
-        	
-        	
-        	System.out.println("Enter quantity to add product\n");
-    		double qty = sc.nextDouble();
-    	
-    		
-    		s.addProduct(allProducts.get(prodIndex).getKey(), qty);
-    		System.out.println("Product added\n");
-    		
-    		System.out.println("Here are your cart products\n");
-    		List<Pair<UUID,Double>> cartItems = s.getCartProducts();
-    		Iterator<Pair<UUID,Double>> itr = cartItems.iterator();
-    	      
-    	      while(itr.hasNext()) {
-    	         Object element = itr.next();
-    	         System.out.print(element + " ");
-    	      }
-        	
-        	
-        	
-        	System.out.println("press one to chekout");
-        	int n = sc.nextInt();
-        	if(n==1) {
-        		
-        		s.checkout(customers,cartItems,deliveryMethod,destAddress,paymentMethod,paymentDetails,paymentConfirmed);
-        	}
-        	
-            FileReader inputFile = new FileReader(filename);
-            try 
-            {
-                Scanner fileReader = new Scanner(inputFile);
-                int memberIndex = 0;
-                while (fileReader.hasNextLine())
-                {
-                    String line = fileReader.nextLine();                     
-                    String lineArray[] = line.split(",");
-                    System.out.println(lineArray[0]);
-                    System.out.println(lineArray[1]);
-                    //addAccount(lineArray[0]);
-                    //getTeams().get(teamIndex).setRanking(Integer.valueOf(lineArray[1]));
-                    memberIndex++;
-                }
-            }
-            finally
-            {
-                inputFile.close();
-            }
-        }
-        catch(FileNotFoundException exception)
-        {
-            System.out.println(filename + " not found");
-        }        
-        catch(IOException exception)
-        {            
-            System.out.println("Unexpected I/O error");
-        }
+        loginUsername = "";
+        loginUser = new UserList();
     }
 
+    public void main() 
+    {
+        Scanner system = new Scanner(System.in); 
+        String menuIndex = "!";
+        do 
+        {
+            switch (menuIndex)
+            {
+                case "!": homePage(); break;
+                case "A": homeUser(); break;
+                case "A1": homeUserLogin(); break;
+                case "A2": homeUserMemberSignup(); break;
+                case "A3": homeUserChangePassword(); break;
+                case "A4": homeUserChangeEmail(); break;
+                case "A5": homeUserChangePhoneNumber(); break;
+                case "A6": homeUserCurrentUnregister(); break;
+                case "A7": homeUserLogout(); break;
+                case "A8": homeUserOwnerSignup(); break;
+                case "A9": homeUserViewUserlist(); break;
+                case "A10": homeUserUnregisterUser(); break;
+                case "X": break;
+            }
+            menuIndex = system.nextLine().toUpperCase().trim();
+        }while (!menuIndex.equals("X"));
+    }
+    
+    public void homePage()
+    {
+        System.out.println("Welcome to Monash FV Store.");
+        System.out.println("");
+        System.out.println("Press A. Account");
+        System.out.println("Press B. To Browse through our products");
+        System.out.println("Press C. View your shopping");
+        System.out.println("Press X: To Exit");
+        System.out.println("");
+    }
+    
+    //option A
+    public void homeUser()
+    {
+        System.out.println("");
+        if (!loginUser.isMemberLogin(loginUsername) && !loginUser.isOwnerLogin(loginUsername))
+        {
+            System.out.println("Press A1. Log in");
+            System.out.println("Press A2. Create your account");
+            System.out.println("Press !. Back to Homepage");
+        }
+        else
+        {
+            System.out.println("Press A3. Change password");
+            System.out.println("Press A4. Change Email");
+            System.out.println("Press A5. Change Phone Number");
+            System.out.println("Press A6. Unregister current account");
+            System.out.println("Press A7. Logout");   
+            System.out.println("");
+        }
+        if (loginUser.isOwnerLogin(loginUsername))
+        {
+            System.out.println("Owner's authority");
+            System.out.println("Press A8 Register owner account");
+            System.out.println("Press A9 View Userlist");
+            System.out.println("Press A10 Unregister user");
+        }
+        System.out.println("");
+    }
+    
+    //option A1
+    public void homeUserLogin()
+    {
+        System.out.println("This is Log in page");        
+        Scanner system = new Scanner(System.in); 
+        System.out.print("Please enter your username: ");        
+        String enterUsername = system.nextLine().trim();
+        System.out.print("Please enter your password: ");
+        String enterPassword = system.nextLine().trim();
+        if (loginUser.userLogin(enterUsername, enterPassword))
+        {
+           loginUsername = enterUsername;
+           System.out.println("");
+           System.out.println("Successful");
+           homeUser();
+        }
+        else
+        {
+            System.out.println("");
+            System.out.println("Error! Please try again.");
+            homeUserLogin();
+        }        
+    }
+    
+    //option A2
+    public void homeUserMemberSignup()
+    {
+        Scanner system = new Scanner(System.in); 
+        System.out.print("Please enter your username: ");        
+        String username = system.nextLine().trim();
+        System.out.print("Please enter your password: ");
+        String password = system.nextLine().trim();
+        System.out.print("Please enter your first name: ");
+        String firstName = system.nextLine().trim();
+        System.out.print("Please enter your last name: ");
+        String lastName = system.nextLine().trim();
+        System.out.print("Please enter your date of birth: ");
+        Calendar dob = Calendar.getInstance();
+        System.out.print("Please enter your phone number: ");
+        String phoneNumber = system.nextLine().trim();
+        System.out.print("Please enter your email: ");
+        String email = system.nextLine().trim();
+        if (loginUser.registerMember(username, password, firstName, lastName, dob, 
+                            phoneNumber, email))
+        {
+            System.out.println("");
+            System.out.println("Thank you for joinning us.");
+            homeUser();
+        }
+        else
+        {
+            System.out.println("");
+            System.out.println("Error! Please try again.");
+            homeUserMemberSignup();
+        }
+    }
+    
+    //option A3
+    public void homeUserChangePassword()
+    {
+        Scanner system = new Scanner(System.in); 
+        System.out.print("Please enter your new password: "); 
+        String newPassword = system.nextLine().trim();
+        if (loginUser.changePassword(loginUsername, newPassword))
+        {
+            System.out.println("");
+            System.out.println("Your password has been changed.");
+            homeUser();
+        }
+        else
+        {
+            System.out.println("");
+            System.out.println("Error! Please try again.");
+            homeUserChangePassword();
+        }        
+    }
+    
+    //option A4
+    public void homeUserChangeEmail()
+    {
+        Scanner system = new Scanner(System.in); 
+        System.out.print("Please enter your new email: "); 
+        String newEmail = system.nextLine().trim();
+        if (loginUser.changeEmail(loginUsername, newEmail))
+        {
+            System.out.println("");
+            System.out.println("Your email has been changed.");
+            homeUser();
+        }
+        else
+        {
+            System.out.println("");
+            System.out.println("Error! Please try again.");
+            homeUserChangeEmail();
+        }        
+    }
+    
+    //option A5
+    public void homeUserChangePhoneNumber()
+    {
+        Scanner system = new Scanner(System.in); 
+        System.out.print("Please enter your new phone number: "); 
+        String newPhoneNumber = system.nextLine().trim();
+        if (loginUser.changePhoneNumber(loginUsername, newPhoneNumber))
+        {
+            System.out.println("");
+            System.out.println("Your phone number has been changed.");
+            homeUser();
+        }
+        else
+        {
+            System.out.println("");
+            System.out.println("Error! Please try again.");
+            homeUserChangePhoneNumber();
+        }        
+    }
+    
+    //option A6
+    public void homeUserCurrentUnregister()
+    {
+        if (loginUser.unregisterUser(loginUsername))
+        {
+            System.out.println("");
+            System.out.println("Your account has been deleted.");
+            homeUser();
+        }
+        else
+        {
+            System.out.println("");
+            System.out.println("Error! Please try again.");
+            homeUser();
+        }        
+    }
+    
+    //option A7
+    public void homeUserLogout()
+    {
+        if (loginUser.userLogout(loginUsername))
+        {
+            loginUsername = "";
+            System.out.println("");
+            System.out.println("Log out successfully.");
+            homeUser();
+        }
+        else
+        {
+            System.out.println("");
+            System.out.println("Error! Please try again.");
+            homeUser();
+        }        
+    }
+    
+    //option A8
+    public void homeUserOwnerSignup()
+    {
+        Scanner system = new Scanner(System.in); 
+        System.out.print("Please enter your username: ");        
+        String username = system.nextLine().trim();
+        System.out.print("Please enter your password: ");
+        String password = system.nextLine().trim();
+        System.out.print("Please enter your first name: ");
+        String firstName = system.nextLine().trim();
+        System.out.print("Please enter your last name: ");
+        String lastName = system.nextLine().trim();
+        System.out.print("Please enter your date of birth: ");
+        Calendar dob = Calendar.getInstance();
+        System.out.print("Please enter your phone number: ");
+        String phoneNumber = system.nextLine().trim();
+        System.out.print("Please enter your email: ");
+        String email = system.nextLine().trim();
+        if (loginUser.registerMember(username, password, firstName, lastName, dob, 
+                            phoneNumber, email))
+        {
+            System.out.println("");
+            System.out.println("Another owner account has been created.");
+            homeUser();
+        }
+        else
+        {
+            System.out.println("");
+            System.out.println("Error! Please try again.");
+            homeUserOwnerSignup();
+        }
+    }
+    
+    //option A9
+    public void homeUserViewUserlist()
+    {
+        loginUser.viewUserlist();
+        homeUser();
+    }
+    
+    //option A10
+    public void homeUserUnregisterUser()
+    {
+        System.out.println("User List:");
+        System.out.println("");
+        loginUser.viewUserlist();
+        System.out.println("");
+        Scanner system = new Scanner(System.in); 
+        System.out.print("Please enter the username of the account to be deleted: ");        
+        String username = system.nextLine().trim();
+        if (loginUser.unregisterUser(username))
+        {
+            System.out.println("");
+            System.out.println(username + " account has been deleted.");
+            homeUser();
+        }
+        else
+        {
+            System.out.println("");
+            System.out.println("Error! Please try again.");
+            homeUser();
+        }        
+    }
 }
