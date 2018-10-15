62de27a5e891a5083805fc9b47bd80c18ecf3fa0
harry <harry@118.138.190.89>
2018-10-15 13:18:32 +1100

    harry - menu wording correction

--- src/boundary/Menu.java
+++ src/boundary/Menu.java
@@ -1268,7 +1268,7 @@ public class Menu
 			e.printStackTrace();
 		}
         
- 		System.out.println("Enter end date in 'DD/MM/YYYY' format");
+ 		System.out.println("Enter end date in 'yyyy-MM-dd' format");
  		String endDate = sc.nextLine();
  		
  		Date date2;
